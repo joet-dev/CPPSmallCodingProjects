6cec1d1968d42451a7d25b89f7f8c04b1e033e76
Joseph-Thur <69287038+Zenokue@users.noreply.github.com>
2021-04-22 11:38:40 +1000

    Template for EXAM

---
+++ Exam/Template.cpp
@@ -0,0 +1,41 @@
+/*
+ * Thurlow, Joseph (1137508) - Task 1 (22/04/2021)
+ *
+ * This program is to be used for testing code in the CSC200 EXAM. 
+ */
+
+//Header file declaration
+#include <iostream>
+#include <iomanip>
+#include <sstream> 
+#include <string> 
+
+using namespace std;
+
+// Global declarations
+
+
+// Class declarations & method definition. Format: type name (args);
+
+
+// Main function
+int main() { 
+    cout << "Hello World" << endl;
+
+
+    // For programs with multiple classes use system("pause");
+
+    return 0;
+}
+
+// Method definitions
+
+
+
+// HELP!
+/*
+ * Array format: type name[num] = {value, value1, ...};
+ * 
+ * 
+ * 
+ */
